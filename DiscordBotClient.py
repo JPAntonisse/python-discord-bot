7f5d6325f79fcab0560b854d1498a5d707bc1330
Wesley <w.seubring@gmail.com>
2019-11-21 21:22:28 +0100

    Move anoying echo to command

--- DiscordBotClient.py
+++ DiscordBotClient.py
@@ -3,14 +3,7 @@ import os
 from discord.ext import commands
 
 class DiscordBotClient(commands.Bot):
-    # startup_extensions = ["cogs.spongebobCommand"]
-
-    properties = {
-        'spongebob' : True,
-    }
-
     
-
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
 
@@ -25,21 +18,6 @@ class DiscordBotClient(commands.Bot):
         # Check if message not from bot
         if message.author.bot == False:
             #  Actions based on plain input
-            await self.handle_middleware(message)
+            # await self.handle_middleware(message)
 
             await self.process_commands(message)
-
-
-    async def handle_middleware(self, message):
-        # TODO: Dynamicly add modules here
-        if self.properties['spongebob'] == True:
-            content = message.content
-            anoying_message = ""
-            for i in range(0, len(content)):
-                if i % 2 == 0:
-                    anoying_message += content[i].lower()
-                else:
-                    anoying_message += content[i].upper()
-
-            await message.channel.send(anoying_message)
-                
