28968d39f10b128682cd075f58094caa6768214c
Wesley <w.seubring@gmail.com>
2019-11-21 18:50:48 +0100

    plain bot

---
+++ DiscordBotClient.py
@@ -0,0 +1,41 @@
+import discord
+from discord.ext import commands
+
+class DiscordBotClient(commands.Bot):
+    # startup_extensions = ["cogs.spongebobCommand"]
+
+    properties = {
+        'spongebob' : False,
+    }
+
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+        for extension in self.startup_extensions:
+            try:
+                self.load_extension(extension)
+            except Exception as e:
+                exc = '{}: {}'.format(type(e).__name__, e)
+                print('Failed to load extension {}\n{}'.format(extension, exc))
+
+    async def on_message(self, message):
+        # Check if message not from bot
+        if message.author.bot == False:
+            #  Actions based on plain input
+            await self.handle_middleware(message)
+
+            await self.process_commands(message)
+
+    async def handle_middleware(self, message):
+        # TODO: Dynamicly add modules here
+        if self.properties['spongebob'] == True:
+            content = message.content
+            anoying_message = ""
+            for i in range(0, len(content)):
+                if i % 2 == 0:
+                    anoying_message += content[i].lower()
+                else:
+                    anoying_message += content[i].upper()
+
+            await message.channel.send(anoying_message)
+                
---
+++ main.py
@@ -0,0 +1,7 @@
+from DiscordBotClient import DiscordBotClient
+
+token = "NjQ3MTA0NDY3Nzg2OTI0MDM0.Xda1mw.ez4uRnUsbN8b9-aYWht-4bhTv6A"
+client = DiscordBotClient(command_prefix = "$")
+
+client.run(token)
+
