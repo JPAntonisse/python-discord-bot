20735063c03e3320dc27d6f1aafc7e58dc0ee622
Joey Antonisse <jopiloot@hotmail.com>
2019-11-21 20:46:34 +0100

    changed code

--- app/DiscordBotClient.py
+++ app/DiscordBotClient.py
@@ -2,37 +2,33 @@ import discord
 import os
 from discord.ext import commands
 
+
 class DiscordBotClient(commands.Bot):
     # startup_extensions = ["cogs.spongebobCommand"]
 
     properties = {
-        'spongebob' : True,
+        'spongebob': True,
     }
-
-    
 
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
 
-        for filename in os.listdir('./cogs'):
-            if filename.endswith('.py'):
-                
-                self.load_extension(f'cogs.{filename[:-3]}')
+        for filename in os.listdir('./app/cogs'):
+            if filename.endswith('.py') and filename.find('__') == -1:
+                self.load_extension(f'app.cogs.{filename[:-3]}')
                 print(f"loaded: {filename} ")
-
 
     async def on_message(self, message):
         # Check if message not from bot
-        if message.author.bot == False:
+        if not message.author.bot:
             #  Actions based on plain input
             await self.handle_middleware(message)
 
             await self.process_commands(message)
 
-
     async def handle_middleware(self, message):
         # TODO: Dynamicly add modules here
-        if self.properties['spongebob'] == True:
+        if self.properties['spongebob']:
             content = message.content
             anoying_message = ""
             for i in range(0, len(content)):
@@ -42,4 +38,3 @@ class DiscordBotClient(commands.Bot):
                     anoying_message += content[i].upper()
 
             await message.channel.send(anoying_message)
-                
